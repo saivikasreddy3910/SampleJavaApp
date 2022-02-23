7d8c1982630ff7a462bc991d299c3b3762960219
Sai vikas Reddy <saivikasreddy3910@gmail.com>
2022-02-23 12:16:33 +0530

    Sample program added

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo 
+{
+    public static void main(String[] args)
+    {
+          for(int i=1;i<=10;i++)
+          {
+               System.out.println("hello jenkins");
+           }
+
+
+
+     }
+
+
+}
